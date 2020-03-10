832b05867d1b2080baf7a00cbd7100e985c0f988
Christopher Scott <chris.c.scott@outlook.com>
2020-03-10 14:39:18 -0500

    update address generation function and read function

--- Cache.py
+++ Cache.py
@@ -26,12 +26,16 @@ class Column:
             self.rows.append(row)
 
 
-    def read(self, tag, index, num_bytes):
+    def read(self, tag, index, block_offset, num_bytes):
         read_bytes = num_bytes
-        while read_bytes != 0:
-            read_bytes -= self.rows[index].read(tag, num_bytes)
+        while read_bytes != 0 and tag < self.num_rows:
+            bytes_read_from_row, block_offset  = self.rows[index].read(tag, block_offset, num_bytes)
+            if bytes_read_from_row > 0:
+                read_bytes = read_bytes - bytes_read_from_row
+            else:
+                return (read_bytes, index, block_offset)
             index += 1
-        
+        return (read_bytes, index, block_offset)
 
 class Cache:
     def __init__(
@@ -61,15 +65,46 @@ class Cache:
             col = Column(int(rows), self.block_size)
             self.columns.append(col)
 
+        
+
+    def get_column(self):
+        if self.arg == "RR":
+            col = self.columns[self.next_col_index]
+            self.next_col_index = (self.next_col_index + 1) % len(self.columns)
+            return col
+        else :
+            return self.columns[0]
+
     def claculate_bit_offset(self, address, num_bits):
         bits = address & ((1 << num_bits) - 1)
         address = address >> num_bits
         return (bits, address)
 
-    def read(self, address, num_bytes):
+    def get_address(self, address):
         block_offset, address = self.claculate_bit_offset(address, int(self.block_offset_bits))
         index, address = self.claculate_bit_offset(address, int(self.index_bits))
         tag, address = self.claculate_bit_offset(address, int(self.tag_bits))
 
-        print("Block: %X\nIndex: %X\nTag: %X" % (block_offset, index, tag))
+        return (tag, index, block_offset)
+
+    def create_address(self, tag, index, block_offset):
+        address = 0
+        address |= (tag << int(self.block_offset_bits) + int(self.index_bits))
+        address |= (index << int(self.block_offset_bits))
+        address |= block_offset
+
+        return address
+
+
+    def read(self, address, num_bytes):
+        tag, index, block_offset = self.get_address(address)
+
+        for col in iter(self.columns):
+            read_bytes, index, block_offset = col.read(tag, index, block_offset, num_bytes)
+
+        if read_bytes != 0:
+            self.write()
+            
+
+        
 
