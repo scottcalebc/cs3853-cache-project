95507db698ee377f80f1277d7c8fc3938d608e23
Christopher Scott <chris.c.scott@outlook.com>
2020-04-02 18:39:17 -0500

    Update Cache.py to have string function to represent Cache object and update sim.py formatting of output strings to screen

--- Cache.py
+++ Cache.py
@@ -27,37 +27,72 @@ class Column:
 
 
 
-class Cache:
+class Cache():
+
     def __init__(
         self,
-        address_space=1024,
-        size = 1024,
+        size = 1,
         associativity = 1,
         block_size = 16,
         alg = "RR",
     ):
 
-        self.size = size
+        self.size = size * 2**10
         self.associativity = associativity
         self.block_size = block_size
         self.alg = alg
+        self.address_bits = 32
 
         self.next_col_index = 0
         self.columns = []
         rows = self.size / (self.block_size * self.associativity)
 
-        self.block_offset_bits = math.log(float(block_size), 2)
-        self.index_bits = math.log(float(rows), 2)
-        self.tag_bits = math.log(float(address_space), 2) - self.index_bits - self.block_offset_bits
+        self.block_offset_bits = int(math.log(float(block_size), 2))
+        self.index_bits = int(math.log(float(rows), 2))
+        self.tag_bits = self.address_bits - self.index_bits - self.block_offset_bits
+        
+        self.overhead_bytes = int(self.associativity * (1 + self.tag_bits) * rows/8)
+        self.total_memory_size = (self.size + self.overhead_bytes)/ 2**10
+
+        self.cost = 0.05
 
 
         for i in range(0, associativity):
             col = Column(int(rows), self.block_size)
             self.columns.append(col)
 
+    def test_str(self):
+        return (
+            f'Size: {self.size}\n'
+            f'block size: {self.block_size}\n'
+            f'Associativity: {self.associativity}\n'
+            f'Replacement Policy: {self.alg}\n'
+            f'Address bits: {self.address_bits}'
+        )
+
+    def overhead_size(self):
+        return f'{self.overhead_bytes} bytes'
+
+
+    def impl_mem_size(self):
+        return f'{self.total_memory_size} KB ({self.overhead_bytes} bytes)'
+        
         
 
+    def cost_str(self):
+        return "${0:.2f}".format(self.total_memory_size * self.cost)
+        
 
+    def __str__(self):
+        return (
+            f'Total # Blocks:                 {self.size / self.block_size}\n'
+            f'Tag Size:                       {self.tag_bits}\n'
+            f'Index Size:                     {self.index_bits}\n'
+            f'Total # Rows:                   {self.columns[0].num_rows}\n'
+            f'Overhead Size:                  {self.overhead_size()}\n'
+            f'Implementation Memory Size:     {self.impl_mem_size()}\n'
+            f'Cost:                           {self.cost_str()}\n'
+        )
         
             
 
--- sim.py
+++ sim.py
@@ -3,58 +3,51 @@
 
 import sys 
 import math 
-addrSize = 32
-filename = sys.argv[2]
-cacheSize = sys.argv[4]
-blockSize = sys.argv[6]
-associativity = int(sys.argv[8])
-policy = sys.argv[10]
-f = open(filename, 'r')
-print("Cache Simulator CS 3853 Spring 2020")
-print("Cmd Line:" , sys.argv[:])
-print("Trace File:", filename)
-print("Cache Size: ", cacheSize, "KB")
-print("Block Size: ", blockSize, "bytes")
-print("Associativity: ", associativity)
-print("Policy: ", policy)
-print("----- Calculated Values -----")
-if(associativity == 1):
-    #calculate the values for a fully associated cache 
-    index = 0
-    totalIndices = 1
-    offset = math.log(float(blockSize), 2)
-    tagSize = (int)(addrSize - offset)
-    totalBlocks = (int(cacheSize) * 1024 )/int(blockSize)
-    totalBlocksPower = math.log(float(totalBlocks), 2)
-   #overhead = valid bits + tag bits + dirty bits 
-    overhead = (totalBlocks*( 1 + tagSize)) / 8
-    implementationMemory = int(cacheSize)*1024 + int(overhead)
-    hitRate = 0 
-else:
-    # 32  = tag bits + index + offset 
-    # tag bits =  32 - index - offset
-    # offset == block size = n where 2^n
-    offset = math.log(float(blockSize), 2) 
-    associativityPower = math.log(float(associativity), 2)
-    # index == cache size = n where 2^n (the plus 10 is for kb)
-    index = (int)(math.log(float(cacheSize), 2) + 10 - associativityPower - offset)
-    tagSize = (int)(addrSize - index - offset)
-    totalBlocksPower = index + associativityPower
-    totalBlocks = (int)(2 ** totalBlocksPower)
-    totalIndices = (int)(totalBlocks / associativity)
-    #overhead = valid bits + tag bits + dirty bits 
-    overhead = (totalBlocks*( 1 + tagSize)) / 8
-    implementationMemory = int(cacheSize)*1024 + int(overhead)
-    hitRate = 0
+from Cache import Cache
 
-print("offset:", offset)
-print("Total # Blocks:", totalBlocks, "( 2 ^",totalBlocksPower,")")
-print("Tag Size:", tagSize, "bits")
-print("Index Size:", index,"bits, Total Indices:", totalIndices)
-print("Overhead Memory Size:", overhead, "bytes")
-print("Implementation Memory Size:", implementationMemory, "bytes")
-print("----- Results -----")
-print("Cache Hit Rate:", hitRate, "%")
+sys.argv.pop(0)
+
+usage = "python sim.py -f trace_file -s size_in_KB -b block_size -a associativity -r replacment_policy"
+
+
+def get_param_switch(switch):
+    f = sys.argv.index(switch)
+    sys.argv.pop(f)
+    return sys.argv.pop(f)
+
+if len(sys.argv) < 10:
+    print(f'Invalid Parameters:  {usage}')
+    exit(1)
+
+
+try:
+    filename = get_param_switch("-f")
+    cacheSize = int(get_param_switch("-s"))
+    blockSize = int(get_param_switch("-b"))
+    associativity = int(get_param_switch("-a"))
+    policy = get_param_switch("-r")
+
+    if sys.argv:
+        print(f'leftover Parameters:  {usage}')
+        exit(1)
+except Exception:
+    print(f'Unable to convert Parameters:  {usage}')
+    exit(1)
+
+
+f = open(filename, 'r')
+print("Cache Simulator CS 3853 Spring 2020\n")
+print(f'Trace File:                     {filename}\n')
+print("****** Cache Input Parameters ******\n")
+print(f'Cache Size:                     {cacheSize}')
+print(f'Block Size:                     {blockSize}')
+print(f'Associativity:                  {associativity}')
+print(f'Replacement Policy:             {policy}\n')
+print("***** Calculated Values *****\n")
+
+cache = Cache(size=cacheSize, associativity=associativity, block_size=blockSize, alg=policy)
+
+print(cache)
 
 lines = f.readlines()
 stopPoint = range(0,60)
